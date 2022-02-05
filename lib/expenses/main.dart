9d98b212b5a742ef36a72d924a1245b6edda5bf3
Aditya Kumar Sahu <aditya@acceldata.io>
2022-02-05 13:54:56 +0530

    added transactions

--- lib/expenses/main.dart
+++ lib/expenses/main.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +12,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       title: "Expenses App",
       home: MyHomePage(),
@@ -17,6 +20,24 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: '1',
+        title: 'New Shoes',
+        amount: 29.9,
+        date: DateTime(2022, 1, 20, 12, 00)),
+    Transaction(
+        id: '2',
+        title: 'New Bag',
+        amount: 39.9,
+        date: DateTime(2022, 1, 20, 12, 00)),
+    Transaction(
+        id: '3',
+        title: 'New Phone',
+        amount: 299.9,
+        date: DateTime(2022, 1, 20, 12, 00)),
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -25,15 +46,47 @@ class MyHomePage extends StatelessWidget {
         title: Text("Expenses App"),
       ),
       body: Column(
-        children: <Widget>[
-          Card(
-            color: Colors.blue,
-            child: Container(width: double.infinity, child: Text("CHART")),
-          ),
-          Card(
-            child: Text("List of Expenses"),
-          )
-        ],
+        children: this.transactions.map((tx) {
+          return Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Card(
+                child: Container(
+              padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                            color: Colors.teal,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                      Text(
+                        DateFormat().format(tx.date),
+                        style: TextStyle(color: Colors.blueGrey),
+                      )
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "\$ " + tx.amount.toString(),
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
+                  ),
+                ],
+              ),
+            )),
+          );
+        }).toList(),
       ),
     );
   }
